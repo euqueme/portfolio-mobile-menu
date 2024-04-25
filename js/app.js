33d007bc262fdbc0e451f6a02fd0e5f961162fe7
María Quemé <euqueme@gmail.com>
2024-04-24 23:40:06 -0600

    Add menu showing and closing basic actions

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const menuBtn = document.querySelector(".menu-btn");
+const btnLines = document.querySelectorAll(".btn-line");
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu-item");
+
+// Set the initial state of the menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        // Transform Hamburger to X button
+        menuBtn.classList.add("close");
+        btnLines.forEach((item) =>
+            item.classList.add("close"));
+            
+        // Show menu
+        menu.classList.add("show");
+        menuItems.forEach((item) =>
+            item.classList.add("show"));
+            
+        // Reset the menu state
+        showMenu = true;
+    } else {
+        // Transform X to Hamburger button
+        menuBtn.classList.remove("close");
+        btnLines.forEach((item) =>
+        item.classList.remove("close"));
+        
+        // Hide menu
+        menu.classList.remove("show");
+        menuItems.forEach((item) =>
+            item.classList.remove("show"));
+        
+        // Reset the menu state
+        showMenu = false;
+    }
+}
