dbfb7a3b0934400b8c9e0cf7b4a0b083fdf1a91c
María Quemé <euqueme@gmail.com>
2024-04-28 23:02:06 -0600

    Fix some js styles

--- js/app.js
+++ js/app.js
@@ -3,39 +3,39 @@ const btnLines = document.querySelectorAll('.btn-line');
 const menu = document.querySelector('.menu');
 const menuItems = document.querySelectorAll('.menu-item');
 
-function toggleMenu() {
-    if (!showMenu) {
-        // Transform Hamburger to X button
-        menuBtn.classList.add('close');
-        btnLines.forEach((item) =>
-            item.classList.add('close'));
-            
-        // Show menu
-        menu.classList.add('show');
-        menuItems.forEach((item) =>
-            item.classList.add('show'));
-            
-        // Reset the menu state
-        showMenu = true;
-    } else {
-        // Transform X to Hamburger button
-        menuBtn.classList.remove('close');
-        btnLines.forEach((item) =>
-        item.classList.remove('close'));
-        
-        // Hide menu
-        menu.classList.remove('show');
-        menuItems.forEach((item) =>
-            item.classList.remove('show'));
-        
-        // Reset the menu state
-        showMenu = false;
-    }
-}
-
 // Set the initial state of the menu
 let showMenu = false;
 
+function toggleMenu() {
+  if (!showMenu) {
+    // Transform Hamburger to X button
+    menuBtn.classList.add('close');
+    btnLines.forEach((item) =>
+    item.classList.add('close'));
+            
+    // Show menu
+    menu.classList.add('show');
+    menuItems.forEach((item) =>
+      item.classList.add('show'));
+            
+    // Reset the menu state
+    showMenu = true;
+  } else {
+    // Transform X to Hamburger button
+    menuBtn.classList.remove('close');
+    btnLines.forEach((item) =>
+    item.classList.remove('close'));
+        
+    // Hide menu
+    menu.classList.remove('show');
+    menuItems.forEach((item) =>
+    item.classList.remove('show'));
+        
+    // Reset the menu state
+    showMenu = false;
+  }
+}
+
 menuBtn.addEventListener('click', toggleMenu);
 menuItems.forEach((item) =>
-    item.addEventListener('click', toggleMenu));
+  item.addEventListener('click', toggleMenu));
